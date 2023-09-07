ac1130700add0cc5083c1ea411ea9e39aee86c82
Hai Le <haile.vnm@gmail.com>
2023-09-07 13:29:39 +0700

    chore: fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
@@ -22,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,7 +1,9 @@
-import { ReportHandler } from 'web-vitals';
+import { type ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
