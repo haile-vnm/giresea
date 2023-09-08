86af214c78033ce1661e9f79231ad534a09acea0
Hai Le <haile.vnm@gmail.com>
2023-09-08 12:36:43 +0700

    feat: support navigate pages

---
+++ src/components/unknown-size-pagination/index.tsx
@@ -0,0 +1,80 @@
+import { Pagination, PaginationProps } from 'antd';
+
+const PAGE_ITEMS = 10;
+const PAGE_SIZES = 3;
+const TOTAL_ITEMS = PAGE_SIZES * PAGE_ITEMS; 
+// pageIndex: 1 - 2 - 3
+const PAGE_INDEX = {
+  first: 1,
+  center: 2,
+  last: 3 
+};
+
+interface PageInfo {
+  endCursor?: string;
+  hasNextPage?: boolean;
+}
+
+interface UnknownSizePaginationProps extends PaginationProps {
+  pageInfo?: PageInfo;
+  onPrev?: (token: string) => void;
+  onNext?: (token: string) => void;
+}
+
+const itemRender: PaginationProps['itemRender'] = (_, type) => {
+  if (type === 'prev') {
+    return <a>Previous</a>;
+  }
+  if (type === 'next') {
+    return <a>Next</a>;
+  }
+  return;
+};
+
+const getCurrentPageIndex = (pageInfo?: PageInfo) => {
+  const enableNext = pageInfo?.hasNextPage;
+  const enablePrev = pageInfo?.endCursor;
+
+  if (enablePrev && enableNext) {
+    return PAGE_INDEX.center;
+  }
+
+  if (enablePrev) {
+    return PAGE_INDEX.last;
+  }
+
+  if (enableNext) {
+    return PAGE_INDEX.first;
+  }
+
+  return;
+};
+
+export default function UnknownSizePagination(props: UnknownSizePaginationProps) {
+  const { pageInfo, onNext, onPrev } = props;
+  const currentPage = getCurrentPageIndex(pageInfo);
+
+  const onChange = (page: number) => {
+    if (page === PAGE_INDEX.last && onNext) {
+      onNext(pageInfo?.endCursor!);
+    }
+
+    if (page === PAGE_INDEX.first && onPrev) {
+      onPrev(pageInfo?.endCursor!);
+    }
+  };
+
+  if (!currentPage) {
+    return;
+  }
+
+  return (
+    <Pagination
+      { ...props }
+      current={currentPage}
+      itemRender={itemRender}
+      onChange={onChange}
+      total={TOTAL_ITEMS}
+    />
+  );
+}
--- src/pages/github-repositories/index.tsx
+++ src/pages/github-repositories/index.tsx
@@ -1,23 +1,27 @@
-import { Pagination } from 'antd';
 import DebounceSearchInput from '../../components/debounce-search-input';
 import GitHubRepositories from '../../components/github-repositories';
 import { useState } from 'react';
 import { useReactRepositorySearch } from '../../integrations/github/repository';
+import UnknownSizePagination from '../../components/unknown-size-pagination';
 
 export default function GitHubRepositoriesPage() {
   const [searchName, setSearchName] = useState('');
   const [page, setPage] = useState({});
   const { data, error, loading } = useReactRepositorySearch({ ...page, search: searchName });
 
-  const paginate = (page: number) => {
-    console.log({ page });
+  const loadNextPage = (token: string) => {
+    setPage({ after: token });
+  };
+
+  const loadPrevPage = (token: string) => {
+    setPage({ before: token });
   };
 
   return (
     <>
       <DebounceSearchInput commit={(value) => setSearchName(value)}></DebounceSearchInput>
       <GitHubRepositories items={data?.repos || []}></GitHubRepositories>
-      <Pagination hideOnSinglePage={true} onChange={paginate} total={50}/>
+      <UnknownSizePagination onNext={loadNextPage} onPrev={loadPrevPage} pageInfo={data.pageInfo}/>
     </>
   );
 }
