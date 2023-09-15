2d4e81665a63ff1796d493a3d3a946d3b2c0d0d9
Hai Le <haile.vnm@gmail.com>
2023-09-15 17:15:37 +0700

    fix: remove unnessary field in repository model

--- src/integrations/github/types/repository.ts
+++ src/integrations/github/types/repository.ts
@@ -1,10 +1,6 @@
 export interface Repository {
-  id: string;
   name: string;
-  avatar: string;
-  description: string;
   forkCount: number;
   stargazerCount: number;
-  projectsUrl: string;
   url: string;
 }
