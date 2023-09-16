668417a2247c626954869b3fc4305d8dcb4744dd
Hai Le <haile.vnm@gmail.com>
2023-09-16 13:53:06 +0700

    fix(debounce-input): do not emit value at the first time

--- src/components/debounce-search-input/index.tsx
+++ src/components/debounce-search-input/index.tsx
@@ -1,31 +1,37 @@
 import { Input } from 'antd';
 import { ChangeEvent, useEffect, useState } from 'react';
-import { useDebounce } from 'usehooks-ts';
+import { useDebounce, useIsFirstRender } from 'usehooks-ts';
 import { USER_TYPING_TIME } from '../../lib/constant';
 
 const { Search } = Input;
 
 interface DebounceInputProps {
   placeholder?: string;
-  time?: number; // in milisecond
+  time?: number; // in milliseconds
   commit?: (value: string) => void;
 }
 
 export default function DebounceSearchInput({ time, commit, placeholder }: DebounceInputProps) {
   const [value, setValue] = useState('');
-  const debouncedValue = useDebounce(value, time || USER_TYPING_TIME);
+  const isFirstRender = useIsFirstRender();
+  const debouncedValue = useDebounce<string>(value, time || USER_TYPING_TIME);
   const onSearch = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setValue(value);
   };
 
-  useEffect(() => commit && commit(debouncedValue), [debouncedValue]);
+  useEffect(() => {
+    if (isFirstRender) {
+      return;
+    }
+
+    commit && commit(debouncedValue);
+  }, [debouncedValue]);
 
   return (
     <Search
       onChange={onSearch}
       placeholder={placeholder}
-      value={value}
     />
   );
 }
