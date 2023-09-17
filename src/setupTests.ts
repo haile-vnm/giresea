bbb5d4a4aa561b89844df58b0f0ba9cc20817a75
Hai Le <haile.vnm@gmail.com>
2023-09-17 14:35:52 +0700

    test: mock localstorage

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,9 +3,37 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
 global.matchMedia = global.matchMedia || function () {
   return {
     addListener: jest.fn(),
     removeListener: jest.fn(),
   };
 };
+const localStorageMock = (function () {
+  let store: Record<string, unknown> = {};
+
+  return {
+    getItem(key: string) {
+      return store[key];
+    },
+
+    setItem(key: string, value: unknown) {
+      store[key] = value;
+    },
+
+    clear() {
+      store = {};
+    },
+
+    removeItem(key: string) {
+      delete store[key];
+    },
+
+    getAll() {
+      return store;
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
